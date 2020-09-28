d7818d6c4da4542bdd49b74ff6b3f89d7ec352f1
Russ <rperry1886@gmail.com>
2020-09-27 21:36:06 -0400

    Updates

---
+++ precache-manifest.234fa48c3e562a42b9011dc4cf682c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95526f9cfbce7baacd511b6d374ab2f7",
+    "url": "/github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bc1b04d80a22a967b2f4",
+    "url": "/github.io/monster-rolodex/static/css/main.304d1495.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/github.io/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1b04d80a22a967b2f4",
+    "url": "/github.io/monster-rolodex/static/js/main.e4e349a9.chunk.js"
+  },
+  {
+    "revision": "7f05c795fca003d60b04",
+    "url": "/github.io/monster-rolodex/static/js/runtime-main.a13a4927.js"
+  }
+]);
